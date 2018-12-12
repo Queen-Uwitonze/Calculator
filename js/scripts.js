31b1657311cc1ddd7de901cb3126b6180bb9c3b5
Queen-Uwitonze <uwitonzeq@gmail.com>
2018-12-12 09:39:28 -0500

    i added a new file for BMI and i modified css,js,script.html

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+  
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result =  multiply(number1, number2);
+  alert(result);
+
+//i am using javascript to calculate body mass index .
+  function bmi(weight, height) {
+    return weight / height * height;
+  };
+  
+  var weight = parseInt(prompt("Enter a weight:"));
+  var height = parseInt(prompt("Enter another height:"));
+  alert(result)
